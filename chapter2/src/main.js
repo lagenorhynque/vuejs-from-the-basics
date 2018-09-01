6c34dae812e866031b07d59b182360557420a2f1
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-01 19:58:56 +0900

    Section 12

--- chapter2/src/main.js
+++ chapter2/src/main.js
@@ -1,18 +1,9 @@
 const app = new Vue({
   el: '#app',
   data: {
-    show: true
-  },
-  mounted: function () {
-    console.log(this.$el);
-    console.log(this.$refs.hello);
-  },
-  methods: {
-    handleClick() {
-      const count = this.$refs.count;
-      if (count) {
-        count.innerText = parseInt(count.innerText, 10) + 1;
-      }
-    }
+    url: 'httP://example.com',
+    message: 'Vue.js!',
+    message2: 'Hello Vue.js!',
+    message3: 'Hello <strong>Vue.js!</strong>'
   }
 });
