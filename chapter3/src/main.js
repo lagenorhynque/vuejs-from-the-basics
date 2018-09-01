fb437a356b2e984a1c3f01c4255122c0d4789eb1
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-01 22:02:36 +0900

    Section 14

--- chapter3/src/main.js
+++ chapter3/src/main.js
@@ -1,17 +1,21 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello!',
+    checkboxVal: true,
+    radioVal: '',
+    selectSingleVal: '',
+    selectMultipleVal: [],
+    preview: '',
+    rangeVal: 50,
+    price: 100
   },
   methods: {
-    handleClick() {
-      alert('Clicked!');
-    },
-    handleInput(event) {
-      this.message = event.target.value;
-    },
-    handler(comment) {
-      console.log(comment);
+    handleChange(event) {
+      const file = event.target.files[0];
+      if (file && file.type.match(/^image\/(png|jpeg)$/)) {
+        this.preview = window.URL.createObjectURL(file);
+      }
     }
   }
 });
