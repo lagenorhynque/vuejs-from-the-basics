594fdacc174350e44e04a15b931f382d5db63f41
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-01 18:06:50 +0900

    Section 08

--- chapter2/src/main.js
+++ chapter2/src/main.js
@@ -1,16 +1,25 @@
-const state = { count: 0 };
-
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Vue.js!',
-    state: state,
-    nextTodoText: '',
-    visitCount: 0,
-    hideCompletedTodos: false,
-    todos: [],
-    error: null
+    message: {
+      value: 'Hello Vue.js!'
+    },
+    list: ['apple', 'banana', 'strawberry'],
+    num: 1,
+    count: 0,
+    classObject: {
+      child: true,
+      'is-active': true
+    },
+    styleObject: {
+      color: 'red',
+      backgroundColor: 'lightgray'
+    },
+    radius: 50
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    }
   }
 });
-
-state.count++;
