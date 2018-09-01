2e942ae6921dbd69ec1f88ceaae1f18da3ee1304
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-01 17:09:27 +0900

    Section 05

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 const app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    show: true,
+    count: 0,
+    list: ['apple', 'banana', 'strawberry']
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target);
+    }
+  }
 });
