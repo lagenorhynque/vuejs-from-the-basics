da677c07fc904cc3d2dc2804abfe0ea33e6223c5
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-01 18:19:00 +0900

    Section 09

--- chapter2/src/main.js
+++ chapter2/src/main.js
@@ -1,25 +1,6 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: {
-      value: 'Hello Vue.js!'
-    },
-    list: ['apple', 'banana', 'strawberry'],
-    num: 1,
-    count: 0,
-    classObject: {
-      child: true,
-      'is-active': true
-    },
-    styleObject: {
-      color: 'red',
-      backgroundColor: 'lightgray'
-    },
-    radius: 50
-  },
-  methods: {
-    increment() {
-      this.count += 1;
-    }
+    ok: false
   }
 });
