19ed0912d2d2a229d7442149837de79c60ca6f2f
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-01 19:33:48 +0900

    Section 10

--- chapter2/src/main.js
+++ chapter2/src/main.js
@@ -1,6 +1,34 @@
 const app = new Vue({
   el: '#app',
   data: {
-    ok: false
-  }
+    name: 'chimera',
+    list: []
+  },
+  created: function () {
+    axios.get('list.json')
+      .then(response => {
+        this.list = response.data;
+        this.list.forEach(item => {
+          this.$set(item, 'active', false);
+        });
+      }).catch(e => {
+        console.error(e);
+      });
+  },
+  methods: {
+    doAdd() {
+      const max = this.list.reduce((a, b) => a.id > b.id ? a.id : b.id, 0);
+      this.list.push({
+        id: max + 1,
+        name: this.name,
+        hp:500
+      });
+    },
+    doRemove(index) {
+      this.list.splice(index, 1);
+    },
+    doAttack(index) {
+      this.list[index].hp -= 10;
+    }
+  },
 });
