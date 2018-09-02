680dd8fdf81e9e2831a5a35708f8efccb41782ce
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-03 00:54:35 +0900

    Section 27

--- chapter5/src/main.js
+++ chapter5/src/main.js
@@ -1,29 +1,32 @@
-Vue.component('my-component', {
-  template: '<div class="my-component">\
-    <p>name.{{ name }} HP.{{ hp }}</p>\
-    <p>name <input v-model="localName"></p>\
-    <p>HP <input size="5" v-model.number="localHp"></p>\
-  </div>',
-  props: {
-    name: String,
-    hp: Number
-  },
-  computed: {
-    localName: {
-      get: function () { return this.name; },
-      set: function (val) { this.$emit('update:name', val); }
-    },
-    localHp: {
-      get: function () { return this.hp; },
-      set: function (val) { this.$emit('update:hp', val); }
-    }
+Vue.component('my-component-a', {
+  template: '<div class="my-component-a">Component A</div>'
+});
+
+Vue.component('my-component-b', {
+  template: '<div class="my-component-b">Component B</div>'
+});
+
+Vue.component('comp-board', {
+  template: '<div>Message Board</div>'
+});
+
+Vue.component('comp-form', {
+  template: '<div>Form<textarea v-model="message"></textarea></div>',
+  data: function () {
+    return { message: '' };
   }
 });
 
 const app = new Vue({
   el: '#app',
   data: {
-    name: 'slime',
-    hp: 100
+    componentTypes: ['my-component-a', 'my-component-b'],
+    current: 0,
+    currentView: 'comp-board'
+  },
+  computed: {
+    component() {
+      return this.componentTypes[this.current];
+    }
   }
 });
