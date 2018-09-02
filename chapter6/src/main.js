7a563990ec664c6497b3e86428eb724fdf9df709
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-03 01:45:45 +0900

    Section 30

--- chapter6/src/main.js
+++ chapter6/src/main.js
@@ -1,7 +1,16 @@
 const app = new Vue({
   el: '#app',
   data: {
-    show: true,
-    count: 0
+    order: false,
+    list: [
+      { id: 1, name: 'apple', price: 100 },
+      { id: 2, name: 'banana', price: 200 },
+      { id: 3, name: 'strawberry', price: 300 }
+    ]
+  },
+  computed: {
+    sortedList() {
+      return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc');
+    }
   }
 });
