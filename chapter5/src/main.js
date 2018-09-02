1f85748bbb7e1c4f3b0bbdb28a80d69e29737dcb
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-03 00:30:16 +0900

    Section 25

--- chapter5/src/main.js
+++ chapter5/src/main.js
@@ -1,78 +1,29 @@
-Vue.component('comp-child', {
-  template: '<p>{{ val }}</p>',
-  props: ['val']
-});
-
-Vue.component('comp-item', {
-  template: '<li>{{ name }} HP.{{ hp }}\
-    <button @click="doAttack">Attack</button></li>',
+Vue.component('my-component', {
+  template: '<div class="my-component">\
+    <p>name.{{ name }} HP.{{ hp }}</p>\
+    <p>name <input v-model="localName"></p>\
+    <p>HP <input size="5" v-model.number="localHp"></p>\
+  </div>',
   props: {
-    id: {
-      type: Number,
-      required: true
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    hp: {
-      type: Number,
-      required: true
-    }
+    name: String,
+    hp: Number
   },
-  methods: {
-    doAttack() {
-      this.$emit('attack', this.id);
-    }
-  }
-});
-
-Vue.component('comp-button', {
-  template: '<button @click="handleClick">Fire event</button>',
-  methods: {
-    handleClick() {
-      this.$emit('childs-event');
-    }
-  }
-});
-
-const bus = new Vue({
-  data: {
-    count: 0
-  }
-});
-
-Vue.component('component-b', {
-  template: '<p>bus: {{ bus.count }}</p>',
   computed: {
-    bus() {
-      return bus.$data;
+    localName: {
+      get: function () { return this.name; },
+      set: function (val) { this.$emit('update:name', val); }
+    },
+    localHp: {
+      get: function () { return this.hp; },
+      set: function (val) { this.$emit('update:hp', val); }
     }
-  },
-  created: function () {
-    bus.$on('bus-event', function () {
-      this.count++;
-    });
   }
 });
 
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!',
-    list: [
-      { id: 1, name: 'slime', hp: 100 },
-      { id: 2, name: 'goblin', hp: 200 },
-      { id: 3, name: 'dragon', hp: 500 }
-    ]
-  },
-  methods: {
-    parentsMethod() {
-      alert('Caught an event!');
-    },
-    handleAttack(id) {
-      const item = this.list.find(el => el.id === id);
-      if (item !== undefined && item.hp >= 10) item.hp -= 10;
-    }
+    name: 'slime',
+    hp: 100
   }
 });
