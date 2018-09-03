0d54221378bfb6370c8d00854d9cc468feb3768d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-09-04 00:30:06 +0900

    Section 48

--- my-app/src/router.ts
+++ my-app/src/router.ts
@@ -1,4 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '@/views/Home.vue';
+import Product from '@/views/Product.vue';
 
 Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/product', component: Product },
+  ],
+});
+export default router;
